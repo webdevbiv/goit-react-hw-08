49168b721e76f0dded41648a29fcaa36b7001e8c
Igor <webdevbiv@gmail.com>
2025-05-02 09:36:33 +0300

    UPD | NotFound

--- src/pages/NotFound/NotFoundPage.jsx
+++ src/pages/NotFound/NotFoundPage.jsx
@@ -1,4 +1,32 @@
-const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+import { Link as RouterLink } from 'react-router-dom';
+import { Link, Typography, Box } from '@mui/material';
+
+const NotFoundPage = ({ isLoggedIn }) => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="100vh"
+      textAlign="center"
+    >
+      <Typography variant="h4" gutterBottom>
+        404 - Page Not Found
+      </Typography>
+      <Typography>
+        {isLoggedIn ? (
+          <Link component={RouterLink} to="/contacts">
+            Go to Contacts
+          </Link>
+        ) : (
+          <Link component={RouterLink} to="/login">
+            Login to continue
+          </Link>
+        )}
+      </Typography>
+    </Box>
+  );
 };
+
 export default NotFoundPage;
